ab5b2396fcaaa44fd48b69f9717eabb7e02e17d2
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-05-21 09:15:46 +0200

    Work on TopSlider

---
+++ Configuration/TypoScript/Gridelements/TopSlider.ts
@@ -0,0 +1,7 @@
+tt_content.gridelements_pi1.20.10.setup {
+	topSlider < lib.gridelements.defaultGridSetup
+	topSlider {
+		wrap.cObject = FLUIDTEMPLATE
+		wrap.cObject.file = EXT:theme_amalia/Resources/Private/Templates/Gridelements/TopSlider.html
+	}
+}
